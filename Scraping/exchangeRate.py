1406801aba04973b14e674b2cf87f13f314e7b5b
hcz2000 <huangchangzhan@hotmail.com>
2018-05-26 17:01:23 +0800

    update

---
+++ Scraping/exchangeRate.py
@@ -0,0 +1,51 @@
+from urllib import request,parse
+from html.parser import HTMLParser
+
+class RatesHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.tags=['div','table','tr','td']
+        self.level=0
+        self.item=[]
+        self.items=[]
+      
+    def handle_starttag(self,tag,attrs):
+        if self.level<len(self.tags) and tag==self.tags[self.level]:
+            if self.level==0:
+                for key,value in attrs:
+                    if key=='class' and value=='BOC_main publish':
+                        self.level=self.level+1
+            else:
+                self.level=self.level+1;
+      
+                
+    def handle_data(self,data):
+        if self.level==4:
+            self.item.append(data)
+
+    def handle_endtag(self,tag):
+        if self.level>0 and tag==self.tags[self.level-1]:
+            if self.level==3:
+                if len(self.item)>0:
+                    self.items.append(self.item)
+                    self.item=[]
+            self.level=self.level-1
+        
+    def getItems(self):
+        return self.items
+
+
+def getExchangeRate(): 
+    url= 'http://srh.bankofchina.com/search/whpj/search.jsp'
+
+    parms={'erectDate':'21','nothing':'2016-07-12','pjname':'1316'}
+    querystring=parse.urlencode(parms)
+   
+    req=request.urlopen(url,querystring.encode('ascii'))
+    resp=req.read().decode('utf-8')
+
+    parser=RatesHTMLParser()
+    parser.feed(resp)
+    parser.close()
+    return parser.getItems()
+
---
+++ Stock/nets.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html=urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+
+    try:
+        bsObj=BeautifulSoup(html.read())
+        title=bsObj.head.title
+    except AttributeError as e:
+        return None
+    return title
+
+def test():
+    title=getTitle("http://finance.sina.com.cn/roll/2016-11-19/doc-ifxxwsix4111905.shtml")
+    if title==None:
+        print("Title could not be found")
+    else:
+        print(title)
